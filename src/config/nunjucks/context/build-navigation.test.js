30aef148ea3937118eace7a7446d0d72c3a4ce56
Lukasz Tymoszczuk <publicperson@gmail.com>
2026-05-19 14:58:00 +0100

    clean up

--- src/config/nunjucks/context/build-navigation.test.js
+++ src/config/nunjucks/context/build-navigation.test.js
@@ -1,89 +1,30 @@
 import { buildNavigation } from './build-navigation.js'
 
-function mockRequest(options) {
-  return { ...options }
+const navItems = [
+  { text: 'Home', href: '/' },
+  { text: 'Large logs', href: '/logs' },
+  { text: 'Kill switch', href: '/kill' }
+]
+
+function expectedNav(currentHref) {
+  return navItems.map((item) => ({
+    ...item,
+    current: item.href === currentHref
+  }))
 }
 
 describe('#buildNavigation', () => {
   test('Should provide expected navigation details', () => {
-    expect(
-      buildNavigation(mockRequest({ path: '/non-existent-path' }))
-    ).toEqual([
-      {
-        current: false,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: false,
-        text: 'Large logs',
-        href: '/logs'
-      },
-      {
-        current: false,
-        text: 'Kill switch',
-        href: '/kill'
-      }
-    ])
+    expect(buildNavigation({ path: '/non-existent-path' })).toEqual(
+      expectedNav(null)
+    )
   })
 
-  test('Should provide expected highlighted navigation details', () => {
-    expect(buildNavigation(mockRequest({ path: '/' }))).toEqual([
-      {
-        current: true,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: false,
-        text: 'Large logs',
-        href: '/logs'
-      },
-      {
-        current: false,
-        text: 'Kill switch',
-        href: '/kill'
-      }
-    ])
-  })
-
-  test('Should highlight Large logs nav item when on logs page', () => {
-    expect(buildNavigation(mockRequest({ path: '/logs' }))).toEqual([
-      {
-        current: false,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: true,
-        text: 'Large logs',
-        href: '/logs'
-      },
-      {
-        current: false,
-        text: 'Kill switch',
-        href: '/kill'
-      }
-    ])
-  })
-
-  test('Should highlight Kill switch nav item when on kill page', () => {
-    expect(buildNavigation(mockRequest({ path: '/kill' }))).toEqual([
-      {
-        current: false,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: false,
-        text: 'Large logs',
-        href: '/logs'
-      },
-      {
-        current: true,
-        text: 'Kill switch',
-        href: '/kill'
-      }
-    ])
+  test.each([
+    ['Home', '/'],
+    ['Large logs', '/logs'],
+    ['Kill switch', '/kill']
+  ])('Should highlight %s nav item when on %s page', (_, path) => {
+    expect(buildNavigation({ path })).toEqual(expectedNav(path))
   })
 })
